2018379a194816cbafb86f0908cabdb786ffdcc1
sonkich <adyulgerski94@gmail.com>
2025-06-16 18:39:56 +0300

    Create initial look

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+        <div className="inner-wrapper">
+            <header>
+                <h3 className="title">МЕНЮ</h3>
+            </header>
+            <div>
+                {/*Тука можеш да си слагаш компнентите вече*/}
+            </div>
+        </div>
     </div>
   );
 }
