819872d5624c7abe3f69e1faeb384d1113f6541b
sonkich <adyulgerski94@gmail.com>
2025-06-24 10:15:27 +0300

    Add menu items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import './App.scss';
+import pages from './data/products.json'
+import menuIcon from './images/menu_icon.svg';
+import globeIcon from './images/globe.png';
+import arrow from './images/arrow.png';
+import swipe from './images/swipe.png';
+import { useState } from 'react';
 
 function App() {
+    const [activePage, setActivePage] = useState(pages[0]);
+
   return (
     <div className="wrapper">
         <div className="inner-wrapper">
             <header>
+                <img src={globeIcon} alt="Globe icon"/>
                 <h3 className="title">МЕНЮ</h3>
+                <img src={menuIcon} alt="Menu icon"/>
             </header>
-            <div>
-                {/*Тука можеш да си слагаш компнентите вече*/}
+            <div className="menu">
+                {activePage.items.map((item) => {
+                    return <div className="category">
+                        <p className="category-title">{item.category}</p>
+                        {
+                            item.products.map(product => {
+                              return <div className="product">
+                                  <div className="product-name">
+                                      <p>{product.name} - <span>{product.volume}</span></p>
+                                      {  product.ingredients &&
+                                          <p className="ingredients">{product.ingredients}</p>
+                                      }
+                                  </div>
+                                  <div className="price">
+                                      {product.price}лв.
+                                  </div>
+                              </div>
+                            })
+                        }
+                    </div>
+                })}
+            </div>
+            <div className="actions">
+                <img src={arrow} className="left-arrow" alt="Arrow icon"/>
+                <img src={swipe} alt="Swipe indicator icon"/>
+                <img src={arrow} alt="Arrow icon"/>
             </div>
         </div>
     </div>
