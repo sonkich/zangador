79a6e1a2424fcaf5d6a7241232f4ef6b2f92d6b6
sonkich <adyulgerski94@gmail.com>
2025-06-26 09:02:33 +0300

    Add menu functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { useSwipeable } from 'react-swipeable';
 function App() {
     const [activePage, setActivePage] = useState(pages[0]);
     const [currentPage, setCurrentPage] = useState(0);
+    const [showMenu, setShowMenu] = useState(false);
 
     const handlers = useSwipeable({
         onSwipedLeft: () => turnPage('right'),
@@ -34,15 +35,24 @@ function App() {
         setCurrentPage(number);
     }
 
+    function toggleMenu() {
+        setShowMenu(!showMenu);
+    }
+
     return (
         <div className="wrapper" {...handlers}>
             <div className="inner-wrapper">
+                {
+                    showMenu ? <div className="menu">
+                        <p onClick={() => toggleMenu()}>Затвори</p>
+                    </div> : null
+                }
                 <header>
                     <img src={globeIcon} alt="Globe icon" />
                     <h3 className="title">МЕНЮ</h3>
-                    <img src={menuIcon} alt="Menu icon" />
+                    <img src={menuIcon} onClick={() => toggleMenu()} alt="Menu icon" />
                 </header>
-                <div className="menu">
+                <div className="item-container">
                     {activePage.items.map((item) => {
                         return <div className='category' key={item.category}>
                             <p className="category-title">{item.category}</p>
