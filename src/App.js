b8009ddf4c3aa9cd41b831e944f784c5c65f3eaf
StanGergov <81183207+StanGergov@users.noreply.github.com>
2025-06-26 15:25:29 +0300

    changed menu style

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
             <div className="inner-wrapper">
                 {
                     showMenu ? <div className="menu">
-                        <p onClick={() => toggleMenu()} className="close-btn"><img src={closeIcon} alt="Close icon" /></p>
+                        <span onClick={() => toggleMenu()} className="close-btn"><img src={closeIcon} alt="Close icon" /></span>
                         <div className="categories-container">
                             {
                                 pages.map(page => {
@@ -58,7 +58,6 @@ function App() {
                                 })
                             }
                         </div>
-
                     </div> : null
                 }
                 <header>
@@ -99,123 +98,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-// import './App.scss';
-// import pages from './data/products.json'
-// import menuIcon from './images/menu_icon.svg';
-// import globeIcon from './images/globe.png';
-// import arrow from './images/arrow.png';
-// import swipe from './images/swipe.png';
-// import { useState } from 'react';
-// []
-
-// function App() {
-//     const [activePage, setActivePage] = useState(pages[0]);
-//     const [currentPage, setCurrentPage] = useState(0);
-
-//     let categoriesArr = [];
-//     pages.forEach(page => categoriesArr.push(...page.categories));
-
-//     document.addEventListener('touchstart', handleTouchStart, false);
-//     document.addEventListener('touchmove', handleTouchMove, false);
-
-//     let xDown = null;
-
-//     function getTouches(evt) {
-//         return evt.touches || evt.originalEvent.touches;
-//     }
-
-//     function handleTouchStart(evt) {
-//         const firstTouch = getTouches(evt)[0];
-//         xDown = firstTouch.clientX;
-//     };
-
-//     function handleTouchMove(evt) {
-//         if (!xDown) {
-//             return;
-//         }
-
-//         let xUp = evt.touches[0].clientX;
-//         let xDiff = xDown - xUp;
-
-//         if (xDiff > 5) {
-//             turnPage('right');
-//         } else if (xDiff < -5) {
-//             turnPage('left')
-//         }
-
-//         xDown = null;
-//     };
-
-//     function turnPage(direction) {
-//         if (direction === 'left') {
-//             if (currentPage === 0) return;
-//             setActivePage(pages[currentPage - 1]);
-//             setCurrentPage(currentPage - 1);
-//         } else if (direction === 'right') {
-//             if (currentPage === pages.length - 1) return;
-//             setActivePage(pages[currentPage + 1]);
-//             setCurrentPage(currentPage + 1);
-//         }
-
-//     }
-
-//     function findPage(category) {
-//         setActivePage(pages.find(page => page.categories.includes(category)));
-//     }
-
-//     return (
-//         <div className="wrapper">
-//             <div className="inner-wrapper">
-//                 <header>
-//                     <img src={globeIcon} alt="Globe icon" />
-//                     <h3 className="title">МЕНЮ</h3>
-//                     <div class="btn-group">
-//                         <button type="Dropdown" class="btn btn-secondary" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-//                             <img src={menuIcon} alt="Menu icon" />
-//                         </button>
-//                         <div class="dropdown-menu dropdown-menu-right">
-//                             {
-//                                 categoriesArr.map(category => {
-//                                     return <button onClick={() => findPage(category)} class="dropdown-item" type="button" name={category}>{category}</button>
-//                                 })
-//                             }
-//                         </div>
-//                     </div>
-//                 </header>
-//                 <div className="menu">
-//                     {activePage.items.map((item) => {
-//                         return <div className="category">
-//                             <p className="category-title">{item.category}</p>
-//                             {
-//                                 item.products.map(product => {
-//                                     return <div className="product">
-//                                         <div className="product-name">
-//                                             <p>{product.name} - <span>{product.volume}</span></p>
-//                                             {product.ingredients &&
-//                                                 <p className="ingredients">{product.ingredients}</p>
-//                                             }
-//                                         </div>
-//                                         <div className="price">
-//                                             {product.price}лв.
-//                                         </div>
-//                                     </div>
-//                                 })
-//                             }
-//                         </div>
-//                     })}
-//                 </div>
-//                 <div className="actions">
-//                     <img src={arrow} onClick={() => turnPage('left')} className="left-arrow" alt="Arrow icon" />
-//                     <img src={swipe} alt="Swipe indicator icon" />
-//                     <img src={arrow} onClick={() => turnPage('right')} alt="Arrow icon" />
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default App;
